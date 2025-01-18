7f5fd255e51f95f9b6cf0e62b98b0326bc176a5f
STMA Robotics <booster.club@stmarobotics.org>
2025-01-18 14:01:41 -0600

    Fixed comment formatting

--- src/main/java/frc/robot/subsystems/algaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/algaeSubsystem.java
@@ -5,53 +5,66 @@
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.AlgaeSubsystemConstants.DEVICE_ID_ROLLERMOTOR;
-import static frc.robot.Constants.AlgaeSubsystemConstants.intakeSpeed;
-import static frc.robot.Constants.AlgaeSubsystemConstants.outtakeSpeed;
 
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class algaeSubsystem extends SubsystemBase {
-  
-  //define motors
+/**
+ * Algae subsystem does all jobs related to algae (intake, scoring, etc).
+ */
+public class AlgaeSubsystem extends SubsystemBase {
+
+  // define motors
   public final TalonFX rollerMotor = new TalonFX(DEVICE_ID_ROLLERMOTOR);
   public final TalonFX wristMotor = new TalonFX(DEVICE_ID_ROLLERMOTOR);
-  //define how motors are controlled
+  // define how motors are controlled
   private final VelocityTorqueCurrentFOC rollerControl = new VelocityTorqueCurrentFOC(0.0);
   private final VelocityTorqueCurrentFOC wristControl = new VelocityTorqueCurrentFOC(0.0);
-  
-  //leave, score coral holding, go back to human player to get coral and score it if time permits i.e cycle as many coral as you can
 
-  public algaeSubsystem() {}
-  
+  public AlgaeSubsystem() {
+  }
+
+  /**
+   * Activates motor to activate rollers and intake algae
+   */  
   public void intake() {
     rollerMotor.setControl(rollerControl.withVelocity(Constants.AlgaeSubsystemConstants.intakeSpeed));
   }
-  
-  //might want different outtake and scoring speeds, keep in mind for later
+
+  /**
+   * Reverses motors to push it out
+   */
   public void outtake() {
     rollerMotor.setControl(rollerControl.withVelocity(Constants.AlgaeSubsystemConstants.outtakeSpeed));
   }
 
+  /**
+   * Stops motors entirely
+   */
   public void stop() {
     rollerMotor.stopMotor();
   }
 
-  //score function probably has to be different from outtake due to speeds of rollers
+  /**
+   * Scores intake, does same thing as outtake but at a different speed
+   */
   public void score() {
     rollerMotor.setControl(rollerControl.withVelocity(Constants.AlgaeSubsystemConstants.scoreSpeed));
   }
 
+  /**
+   * Moves intake to the floor position in order to pick up
+   */
   public void moveIntakeDown() {
     wristMotor.setControl(wristControl.withVelocity(Constants.AlgaeSubsystemConstants.wristDownSpeed));
   }
 
+  /**
+   * Moves intake to the upwards position for holding/scoring
+   */
   public void moveIntakeUp() {
     wristMotor.setControl(wristControl.withVelocity(Constants.AlgaeSubsystemConstants.wristUpSpeed));
   }
 }
-
-
