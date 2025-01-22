769ae5c6e124e83563374e11eb36e7431b23cb26
Owen Rossing <151468667+ME6A-PL1ER@users.noreply.github.com>
2025-01-22 16:56:30 -0600

    general climb subsystem fixes

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -6,13 +6,11 @@ import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Millimeters;
 import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.Volts;
 import static frc.robot.Constants.ClimbConstants.CAGE_DETECTION_THRESHOLD_DISTANCE;
 import static frc.robot.Constants.ClimbConstants.CLIMB_LIMIT_FORWARD;
 import static frc.robot.Constants.ClimbConstants.CLIMB_LIMIT_REVERSE;
 import static frc.robot.Constants.ClimbConstants.CLIMB_MAGNETIC_OFFSET;
 import static frc.robot.Constants.ClimbConstants.CLIMB_ROTOR_TO_SENSOR_RATIO;
-import static frc.robot.Constants.ClimbConstants.CLIMB_SLOT_CONFIGS;
 import static frc.robot.Constants.ClimbConstants.CLIMB_STATOR_CURRENT_LIMIT;
 import static frc.robot.Constants.ClimbConstants.CLIMB_SUPPLY_CURRENT_LIMIT;
 import static frc.robot.Constants.ClimbConstants.DEVICE_ID_CLIMB_DETECTION;
@@ -21,10 +19,8 @@ import static frc.robot.Constants.ClimbConstants.DEVICE_ID_CLIMB_MOTOR_1;
 import static frc.robot.Constants.ClimbConstants.DEVICE_ID_CLIMB_MOTOR_2;
 
 import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANcoder;
@@ -35,7 +31,6 @@ import com.ctre.phoenix6.signals.SensorDirectionValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class ClimbSubsystem extends SubsystemBase {
   private static final CANBus CANIVORE_BUS_NAME = null;
@@ -55,19 +50,7 @@ public class ClimbSubsystem extends SubsystemBase {
   // private final MutableMeasure climbAngle = MutableMeasure.zero(Rotations);
   private final MutAngle climbAngle = Radians.mutable(0);
 
-  private final SysIdRoutine m_sysIdRoutineTranslation = new SysIdRoutine(
-      new SysIdRoutine.Config(
-          null,
-          null,
-          null,
-          // Log state with SignalLogger class
-          state -> SignalLogger.writeString("SysIdTranslation_State", state.toString())),
-      new SysIdRoutine.Mechanism(
-          (volts) -> leftMotor.setControl(climbControl.withOutput(volts.in(Volts))),
-          null,
-          this));
-
-  public void ClimbSubsystem() {
+  public ClimbSubsystem() {
     var climbTalonConfig = new TalonFXConfiguration();
     climbTalonConfig.MotorOutput.NeutralMode = Brake;
     climbTalonConfig.CurrentLimits.StatorCurrentLimit = CLIMB_STATOR_CURRENT_LIMIT.in(Amps);
@@ -77,7 +60,6 @@ public class ClimbSubsystem extends SubsystemBase {
     climbTalonConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
     climbTalonConfig.Feedback.RotorToSensorRatio = CLIMB_ROTOR_TO_SENSOR_RATIO; // 25 rotor turns = 1 shaft turn
     climbTalonConfig.Feedback.FeedbackSensorSource = FusedCANcoder;
-    climbTalonConfig.Slot0 = Slot0Configs.from(CLIMB_SLOT_CONFIGS);
     climbTalonConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
     climbTalonConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = CLIMB_LIMIT_FORWARD.in(Rotations);
     climbTalonConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
@@ -109,9 +91,6 @@ public class ClimbSubsystem extends SubsystemBase {
 
   public boolean cageDetected() {
     var measure = cageDetection.getDistance();
-    if (measure == null) {
-      return false;
-    }
     return (measure.getValueAsDouble() < CAGE_DETECTION_THRESHOLD_DISTANCE.in(Millimeters));
   }
 
