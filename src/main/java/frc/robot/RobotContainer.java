8992332ee5cb55b99488809dc1748494edbdcb97
finnsherman <finnsherman999@gmail.com>
2025-01-23 15:54:36 -0600

    made seperate shuffleboard method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -112,18 +112,23 @@ public class RobotContainer {
         .withPosition(columnIndex, 1);
     tab.add("Indexer Dynam Forward", indexerSubsystem.sysIdBeltDynamicCommand(kForward)).withPosition(columnIndex, 2);
     tab.add("Indexer Dynam Reverse", indexerSubsystem.sysIdBeltDynamicCommand(kReverse)).withPosition(columnIndex, 3);
+  }
 
+  public void elevatorPopulateDashboard() { 
+    var tab = Shuffleboard.getTab("Elevator SysId");
+    int columnIndex = 0;
+      
     // Column 1 Elevator motor 1
     tab.add("Elevator 1 Quasi Forward", armSubsystem.sysIdElevator1QuasistaticCommand(kForward))
-        .withPosition(columnIndex + 1, 0);
+      .withPosition(columnIndex + 1, 0);
     tab.add("Elevator 1 Quasi Reverse", armSubsystem.sysIdElevator1QuasistaticCommand(kReverse))
-        .withPosition(columnIndex + 1, 1);
+      .withPosition(columnIndex + 1, 1);
     tab.add("Elevator 1 Dynam Forward", armSubsystem.sysIdElevator1DynamicCommand(kForward))
-        .withPosition(columnIndex + 1, 2);
+      .withPosition(columnIndex + 1, 2);
     tab.add("Elevator 1 Dynam Reverse", armSubsystem.sysIdElevator1DynamicCommand(kReverse))
-        .withPosition(columnIndex + 1, 3);
+      .withPosition(columnIndex + 1, 3);
 
-    // Column 1 Elevator motor 1
+    // Column 2 Elevator motor 2
     tab.add("Elevator 2 Quasi Forward", armSubsystem.sysIdElevator1QuasistaticCommand(kForward))
         .withPosition(columnIndex + 2, 0);
     tab.add("Elevator 2 Quasi Reverse", armSubsystem.sysIdElevator1QuasistaticCommand(kReverse))
