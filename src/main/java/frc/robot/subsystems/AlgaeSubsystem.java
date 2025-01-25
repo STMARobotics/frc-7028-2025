d7d856df212ad60d644a489319518608cf9c32aa
finnsherman <finnsherman999@gmail.com>
2025-01-25 12:50:55 -0600

    fix merge issues

--- src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
@@ -4,39 +4,14 @@
 
 package frc.robot.subsystems;
 
-<<<<<<< HEAD
-import static frc.robot.Constants.AlgaeConstants.ALGAE_SLOT_CONFIGS;
-=======
 import static com.ctre.phoenix6.signals.NeutralModeValue.Brake;
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Volts;
->>>>>>> main
 import static frc.robot.Constants.AlgaeConstants.DEVICE_ID_CANCODER;
 import static frc.robot.Constants.AlgaeConstants.DEVICE_ID_CANRANGE;
 import static frc.robot.Constants.AlgaeConstants.DEVICE_ID_ROLLERMOTOR;
 import static frc.robot.Constants.AlgaeConstants.DEVICE_ID_WRISTMOTOR;
-<<<<<<< HEAD
-import static frc.robot.Constants.AlgaeConstants.INTAKE_SPEED;
-import static frc.robot.Constants.AlgaeConstants.OUTTAKE_SPEED;
-import static frc.robot.Constants.AlgaeConstants.SCORE_SPEED;
-import static frc.robot.Constants.AlgaeConstants.WRIST_DOWN_POSITION;
-import static frc.robot.Constants.AlgaeConstants.WRIST_UP_POSITION;
-
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.CANrangeConfiguration;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.CANrange;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-=======
 import static frc.robot.Constants.AlgaeConstants.EJECT_SPEED;
 import static frc.robot.Constants.AlgaeConstants.INTAKE_SPEED;
 import static frc.robot.Constants.AlgaeConstants.ROLLER_STATOR_CURRENT_LIMIT;
@@ -77,7 +52,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
->>>>>>> main
 
 /**
  * Algae subsystem does all jobs related to algae (intake, scoring, etc).
@@ -85,27 +59,16 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 public class AlgaeSubsystem extends SubsystemBase {
 
   // define motors
-<<<<<<< HEAD
-  public final TalonFX rollerMotor = new TalonFX(DEVICE_ID_ROLLERMOTOR);
-  public final TalonFX wristMotor = new TalonFX(DEVICE_ID_WRISTMOTOR);
-  // define how motors are controlled
-  private final VelocityTorqueCurrentFOC rollerControl = new VelocityTorqueCurrentFOC(0.0);
-  private final PositionVoltage wristControl = new PositionVoltage(0.0);
-=======
   public final TalonFX rollerMotor = new TalonFX(DEVICE_ID_ROLLERMOTOR, CANIVORE_BUS_NAME);
   public final TalonFX wristMotor = new TalonFX(DEVICE_ID_WRISTMOTOR, CANIVORE_BUS_NAME);
   // define how motors are controlled
   private final VelocityTorqueCurrentFOC rollerControl = new VelocityTorqueCurrentFOC(0.0);
   private final MotionMagicVoltage wristControl = new MotionMagicVoltage(0.0).withEnableFOC(true);
->>>>>>> main
   // define cancoder
   public final static CANcoder cancoder = new CANcoder(DEVICE_ID_CANCODER);
   // define canrange
   public final static CANrange canrange = new CANrange(DEVICE_ID_CANRANGE);
 
-<<<<<<< HEAD
-  // variables (yet to actually be used anywhere)
-=======
   private final TorqueCurrentFOC rollerCharacterization = new TorqueCurrentFOC(0.0);
   private final VoltageOut voltageCharacterization = new VoltageOut(0.0).withEnableFOC(true);
 
@@ -133,26 +96,11 @@ public class AlgaeSubsystem extends SubsystemBase {
           output -> wristMotor.setControl(voltageCharacterization.withOutput(output)),
           null,
           this));
->>>>>>> main
 
   // range keeps track of algae distance
   public StatusSignal<Distance> range = canrange.getDistance();
 
   // angle keeps track of subsystem angle to control the wrist
-<<<<<<< HEAD
-  public StatusSignal<Angle> angle = cancoder.getPosition();
-
-  public AlgaeSubsystem() {
-    // motor configs
-    var rollerMotorConfig = new TalonFXConfiguration();
-    var wristMotorConfig = new TalonFXConfiguration();
-
-    rollerMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    wristMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-
-    rollerMotorConfig.Slot0 = Slot0Configs.from(ALGAE_SLOT_CONFIGS);
-    wristMotorConfig.Slot0 = Slot0Configs.from(ALGAE_SLOT_CONFIGS);
-=======
   public StatusSignal<Angle> wristAngle = cancoder.getPosition();
   public StatusSignal<AngularVelocity> wristVelocity = cancoder.getVelocity();
 
@@ -184,15 +132,12 @@ public class AlgaeSubsystem extends SubsystemBase {
         .withSupplyCurrentLimit(ROLLER_SUPPLY_CURRENT_LIMIT)
         .withSupplyCurrentLimitEnable(true);
     rollerMotorConfig.Slot0 = Slot0Configs.from(WRIST_SLOT_CONFIGS);
->>>>>>> main
 
     // can configs
     CANrangeConfiguration configs = new CANrangeConfiguration();
   }
 
   /**
-<<<<<<< HEAD
-=======
    * Creates a command to run the roller dynamic sysid routine
    * 
    * @param direction direction to run the sysid routine
@@ -233,7 +178,6 @@ public class AlgaeSubsystem extends SubsystemBase {
   }
 
   /**
->>>>>>> main
    * Activates motor to activate rollers and intake algae
    */
   public void intake() {
@@ -243,13 +187,8 @@ public class AlgaeSubsystem extends SubsystemBase {
   /**
    * Reverses motors to push it out
    */
-<<<<<<< HEAD
-  public void outtake() {
-    rollerMotor.setControl(rollerControl.withVelocity(OUTTAKE_SPEED));
-=======
   public void eject() {
     rollerMotor.setControl(rollerControl.withVelocity(EJECT_SPEED));
->>>>>>> main
   }
 
   /**
@@ -261,22 +200,14 @@ public class AlgaeSubsystem extends SubsystemBase {
   }
 
   /**
-<<<<<<< HEAD
-   * Scores intake, does same thing as outtake but at a different speed
-   */
-  public void score() {
-=======
    * Drops the algae to the processor from the rollers. Use {@link #moveToProcessorPosition()} to move the intake to the
    * processor position first.
    */
   public void dropAlgaeToProcess() {
->>>>>>> main
     rollerMotor.setControl(rollerControl.withVelocity(SCORE_SPEED));
   }
 
   /**
-<<<<<<< HEAD
-=======
    * Deploy to the position to score in the processor
    */
   public void moveToProcessorPosition() {
@@ -295,7 +226,6 @@ public class AlgaeSubsystem extends SubsystemBase {
   }
 
   /**
->>>>>>> main
    * Moves intake to the floor position in order to pick up
    */
   public void moveIntakeDown() {
