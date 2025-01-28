795ff6228f89633f7c4dfd05ea71456a30419427
Andrew Gasser <agasser01@hotmail.com>
2025-01-27 18:26:03 -0600

    Fix formatting

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -1,10 +1,8 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.ClimbSubsystem;
-
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ClimbSubsystem;
+import java.util.function.DoubleSupplier;
 
 public class ClimbCommand extends Command {
 
@@ -22,14 +20,12 @@ public class ClimbCommand extends Command {
 
     addRequirements(climbSubsystem);
   }
-  
 
   @Override
   public void execute() {
     climbSubsystem.runFrontClimb(frontClimbSpeed.getAsDouble());
     climbSubsystem.runBackClimb(backClimbSpeed.getAsDouble());
   }
-
 
   @Override
   public void end(boolean interrupted) {
