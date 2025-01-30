f2dfdb13c9f72fa63b30ca93ffafa74c3faa0f3c
finnsherman <157659916+finnsherman@users.noreply.github.com>
2025-01-30 10:31:35 -0600

    fix naming

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -137,15 +137,15 @@ public class RobotContainer {
     var tab = Shuffleboard.getTab("Testing");
 
     tab.add("Run tests", testMode.testCommand());
-    tab.addBoolean("Indexer Fowards Test", () -> testMode.getIndexerForwardsTest());
-    tab.addBoolean("Indexer Backwards Test", () -> testMode.getIndexerBackwardsTest());
-    tab.addBoolean("Manipulator Forwards Test", () -> testMode.getManipulatorForwardsTest());
-    tab.addBoolean("Manipulator Backwards Test", () -> testMode.getManipulatorBackwardsTest());
-    tab.addBoolean("Algae Rollers Forwards Test", () -> testMode.getAlgaeRollersForwardsTest());
-    tab.addBoolean("Algae Rollers Backwards Test", () -> testMode.getAlgaeRollersBackwardsTest());
-    tab.addBoolean("Algae Intake Up Test", () -> testMode.getAlgaeIntakeUpTest());
-    tab.addBoolean("Algae Intake Down Test", () -> testMode.getAlgaeIntakeDownTest());
-    tab.addBoolean("Arm Elevator Test", () -> testMode.getArmElevatorTest());
-    tab.addBoolean("Arm Test", () -> testMode.getArmTest());
+    tab.addBoolean("Indexer Fowards Test", () -> testMode.getIndexerForwardsTestResult());
+    tab.addBoolean("Indexer Backwards Test", () -> testMode.getIndexerBackwardsTestResult());
+    tab.addBoolean("Manipulator Forwards Test", () -> testMode.getManipulatorForwardsTestResult());
+    tab.addBoolean("Manipulator Backwards Test", () -> testMode.getManipulatorBackwardsTestResult());
+    tab.addBoolean("Algae Rollers Forwards Test", () -> testMode.getAlgaeRollersForwardsTestResult());
+    tab.addBoolean("Algae Rollers Backwards Test", () -> testMode.getAlgaeRollersBackwardsTestResult());
+    tab.addBoolean("Algae Intake Up Test", () -> testMode.getAlgaeIntakeUpTestResult());
+    tab.addBoolean("Algae Intake Down Test", () -> testMode.getAlgaeIntakeDownTestResult());
+    tab.addBoolean("Arm Elevator Test", () -> testMode.getArmElevatorTestResult());
+    tab.addBoolean("Arm Test", () -> testMode.getArmTestResult());
   }
 }
