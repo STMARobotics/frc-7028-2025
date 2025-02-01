df088733cdeb18e1368a17cd1f74e9b38389c82d
finnsherman <finnsherman999@gmail.com>
2025-02-01 09:47:52 -0600

    remove unnecessary changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -248,7 +248,6 @@ public class Constants {
     public static final Current STATOR_CURRENT_LIMIT = Amps.of(20);
     public static final Current TORQUE_CURRENT_LIMIT = Amps.of(20);
     public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(10);
-
   }
 
   /**
