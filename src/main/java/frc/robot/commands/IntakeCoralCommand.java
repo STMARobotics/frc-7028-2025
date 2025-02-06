7c4e9f32ea6ca6fd9fa963cdcb6f6d89cf917d72
finnsherman <157659916+finnsherman@users.noreply.github.com>
2025-02-06 13:28:12 -0600

    fix spotless

--- src/main/java/frc/robot/commands/IntakeCoralCommand.java
+++ src/main/java/frc/robot/commands/IntakeCoralCommand.java
@@ -4,9 +4,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.GamePieceManipulatorSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
 
-  /**
-   * Runs the indexer belt and the manipulator wheels to transport the coral to the manipulator
-   */
+/**
+  * Runs the indexer belt and the manipulator wheels to transport the coral to the manipulator
+  */
 public class IntakeCoralCommand extends Command {
 
   private final IndexerSubsystem indexerSubsystem;
