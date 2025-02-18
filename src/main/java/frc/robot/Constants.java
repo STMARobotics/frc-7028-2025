0e91885073a7f355d25fee80ad3a083b66273bdb
finnsherman <157659916+finnsherman@users.noreply.github.com>
2025-02-18 17:47:11 -0600

    Add camera positions (#49)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import static com.ctre.phoenix6.signals.GravityTypeValue.Arm_Cosine;
+import static edu.wpi.first.math.util.Units.degreesToRadians;
+import static edu.wpi.first.math.util.Units.inchesToMeters;
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
@@ -94,12 +96,20 @@ public class Constants {
    * Constants for vision processing
    */
   public static class VisionConstants {
-    public static final String[] CAMERA_NAMES = new String[] { "FrontLeft", "FrontRight", "BackLeft", "BackRight" };
+    public static final String[] CAMERA_NAMES = new String[] { "Left", "Front", "Right", "Back" };
     public static final Transform3d[] ROBOT_TO_CAMERA_TRANSFORMS = new Transform3d[] {
-        new Transform3d(new Translation3d(0.5, 0.5, 0.5), new Rotation3d(0, 0, 0)),
-        new Transform3d(new Translation3d(0.5, -0.5, 0.5), new Rotation3d(0, 0, 0)),
-        new Transform3d(new Translation3d(-0.5, 0.5, 0.5), new Rotation3d(0, 0, 0)),
-        new Transform3d(new Translation3d(-0.5, -0.5, 0.5), new Rotation3d(0, 0, 0)) };
+        new Transform3d(
+            new Translation3d(inchesToMeters(10.846), inchesToMeters(12.472), inchesToMeters(10.775)),
+            new Rotation3d(0, 0, degreesToRadians(90))),
+        new Transform3d(
+            new Translation3d(inchesToMeters(12.610), inchesToMeters(-4.332), inchesToMeters(15.943)),
+            new Rotation3d(0, 0, 0)),
+        new Transform3d(
+            new Translation3d(inchesToMeters(8.286), inchesToMeters(-12.691), inchesToMeters(13.642)),
+            new Rotation3d(0, 0, degreesToRadians(-90))),
+        new Transform3d(
+            new Translation3d(inchesToMeters(-13.472), inchesToMeters(-8.088), inchesToMeters(8.541)),
+            new Rotation3d(0, 0, degreesToRadians(180))) };
 
     public static final AprilTagFieldLayout APRILTAG_FIELD_LAYOUT = AprilTagFieldLayout
         .loadField(AprilTagFields.k2025ReefscapeWelded);
