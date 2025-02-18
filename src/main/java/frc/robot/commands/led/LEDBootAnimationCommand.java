b1feb444caeb725e82c64fd904faf4709de0ab24
brandongasser <bgasser12@gmail.com>
2025-02-17 19:22:34 -0600

    Update boot animation pattern

--- src/main/java/frc/robot/commands/led/LEDBootAnimationCommand.java
+++ src/main/java/frc/robot/commands/led/LEDBootAnimationCommand.java
@@ -1,14 +1,10 @@
 package frc.robot.commands.led;
 
-import static edu.wpi.first.units.Units.Percent;
-import static edu.wpi.first.units.Units.Second;
-
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.LEDSubsystem;
-import java.util.Map;
 
 public class LEDBootAnimationCommand extends Command {
 
@@ -22,14 +18,6 @@ public class LEDBootAnimationCommand extends Command {
   public LEDBootAnimationCommand(LEDSubsystem ledSubsystem) {
     this.ledSubsystem = ledSubsystem;
 
-    // LEDPattern blueBase = LEDPattern.solid(Color.kBlue);
-    // LEDPattern yellowBase = LEDPattern.solid(Color.kYellow);
-    // Map<Double, Color> maskSteps = Map.of(0.0, Color.kWhite, 0.5, Color.kBlack);
-    // LEDPattern mask = LEDPattern.steps(maskSteps).scrollAtRelativeSpeed(Percent.per(Second).of(1 / RUN_TIME));
-    // LEDPattern yellowAnimation = yellowBase.mask(mask);
-
-    // pattern = yellowAnimation.overlayOn(blueBase);
-
     addRequirements(ledSubsystem);
   }
 
@@ -38,14 +26,10 @@ public class LEDBootAnimationCommand extends Command {
     timer.reset();
     timer.start();
 
-    // Map<Double, Color> maskSteps = Map.of(0.0, Color.kWhite, 0.2, Color.kBlack);
-    // pattern = LEDPattern.steps(maskSteps).scrollAtRelativeSpeed(Percent.per(Second).of(80 / RUN_TIME));
-
-    LEDPattern blueBase = LEDPattern.solid(Color.kBlue);
-    Map<Double, Color> maskSteps = Map.of(0.0, Color.kYellow, 0.2, Color.kBlack);
-    pattern = LEDPattern.steps(maskSteps)
-        .scrollAtRelativeSpeed(Percent.per(Second).of(100 / RUN_TIME))
-        .overlayOn(blueBase);
+    pattern = LEDPattern.progressMaskLayer(() -> timer.get() / RUN_TIME)
+        .mask(LEDPattern.progressMaskLayer(() -> 1.2 - (timer.get() / RUN_TIME)).reversed())
+        .mask(LEDPattern.solid(Color.kYellow))
+        .overlayOn(LEDPattern.solid(Color.kBlue));
   }
 
   @Override
@@ -61,6 +45,11 @@ public class LEDBootAnimationCommand extends Command {
   @Override
   public void end(boolean interrupted) {
     timer.stop();
+  }
+
+  @Override
+  public boolean runsWhenDisabled() {
+    return true;
   }
 
 }
