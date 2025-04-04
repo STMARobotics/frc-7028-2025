7cd8a6c117996b4175fccafc2e422be89ee4194e
finnsherman <finnsherman999@gmail.com>
2025-04-04 10:25:02 -0500

    Revert "unifnished"
    This reverts commit ec54bf2adefb0bee4a04afd05bc8de6d94bfeb33.

--- src/main/java/frc/robot/AutoCommands.java
+++ src/main/java/frc/robot/AutoCommands.java
@@ -10,30 +10,13 @@ import static frc.robot.Constants.AlignmentConstants.LATERAL_TARGET_L3_LEFT;
 import static frc.robot.Constants.AlignmentConstants.LATERAL_TARGET_L3_RIGHT;
 import static frc.robot.Constants.AlignmentConstants.LATERAL_TARGET_L4_LEFT;
 import static frc.robot.Constants.AlignmentConstants.LATERAL_TARGET_L4_RIGHT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L1_SCORE_POSES_BLUE;
-import static frc.robot.Constants.AlignmentConstants.REEF_L1_SCORE_POSES_RED;
-import static frc.robot.Constants.AlignmentConstants.REEF_L2_SCORE_POSES_BLUE_LEFT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L2_SCORE_POSES_BLUE_RIGHT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L2_SCORE_POSES_RED_LEFT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L2_SCORE_POSES_RED_RIGHT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L3_SCORE_POSES_BLUE_LEFT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L3_SCORE_POSES_BLUE_RIGHT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L3_SCORE_POSES_RED_LEFT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L3_SCORE_POSES_RED_RIGHT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L4_SCORE_POSES_BLUE_LEFT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L4_SCORE_POSES_BLUE_RIGHT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L4_SCORE_POSES_RED_LEFT;
-import static frc.robot.Constants.AlignmentConstants.REEF_L4_SCORE_POSES_RED_RIGHT;
 import static frc.robot.subsystems.LEDSubsystem.ledSegments;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.AlignToReefCommand;
-import frc.robot.commands.DriveToNearestPose;
 import frc.robot.commands.HoldCoralCommand;
 import frc.robot.commands.IntakeCoralCommand;
 import frc.robot.subsystems.AlignmentSubsystem;
@@ -42,7 +25,6 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.GamePieceManipulatorSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
-import java.util.List;
 import java.util.function.BooleanSupplier;
 import org.photonvision.PhotonCamera;
 
@@ -91,77 +73,6 @@ public class AutoCommands {
     this.ledSubsystem = ledSubsystem;
     this.highCameraForLeft = highCameraForLeft;
     this.highCameraForRight = highCameraForRight;
-
-    publishScoringPoses();
-  }
-
-  /** Sends the reef poses to the dashboard for debugging */
-  private void publishScoringPoses() {
-    var table = NetworkTableInstance.getDefault().getTable("scoring_poses");
-
-    // L4 Blue
-    table.getStructArrayTopic("reef_l4_blue_left", Pose2d.struct)
-        .publish()
-        .set(REEF_L4_SCORE_POSES_BLUE_LEFT.toArray(Pose2d[]::new));
-
-    table.getStructArrayTopic("reef_l4_blue_right", Pose2d.struct)
-        .publish()
-        .set(REEF_L4_SCORE_POSES_BLUE_RIGHT.toArray(Pose2d[]::new));
-
-    // L4 Red
-    table.getStructArrayTopic("reef_l4_red_left", Pose2d.struct)
-        .publish()
-        .set(REEF_L4_SCORE_POSES_RED_LEFT.toArray(Pose2d[]::new));
-
-    table.getStructArrayTopic("reef_l4_red_right", Pose2d.struct)
-        .publish()
-        .set(REEF_L4_SCORE_POSES_RED_RIGHT.toArray(Pose2d[]::new));
-
-    // L3 Blue
-    table.getStructArrayTopic("reef_l3_blue_left", Pose2d.struct)
-        .publish()
-        .set(REEF_L3_SCORE_POSES_BLUE_LEFT.toArray(Pose2d[]::new));
-
-    table.getStructArrayTopic("reef_l3_blue_right", Pose2d.struct)
-        .publish()
-        .set(REEF_L3_SCORE_POSES_BLUE_RIGHT.toArray(Pose2d[]::new));
-
-    // L3 Red
-    table.getStructArrayTopic("reef_l3_red_left", Pose2d.struct)
-        .publish()
-        .set(REEF_L3_SCORE_POSES_RED_LEFT.toArray(Pose2d[]::new));
-
-    table.getStructArrayTopic("reef_l3_red_right", Pose2d.struct)
-        .publish()
-        .set(REEF_L3_SCORE_POSES_RED_RIGHT.toArray(Pose2d[]::new));
-
-    // L2 Blue
-    table.getStructArrayTopic("reef_l2_blue_left", Pose2d.struct)
-        .publish()
-        .set(REEF_L2_SCORE_POSES_BLUE_LEFT.toArray(Pose2d[]::new));
-
-    table.getStructArrayTopic("reef_l2_blue_right", Pose2d.struct)
-        .publish()
-        .set(REEF_L2_SCORE_POSES_BLUE_RIGHT.toArray(Pose2d[]::new));
-
-    // L2 Red
-    table.getStructArrayTopic("reef_l2_red_left", Pose2d.struct)
-        .publish()
-        .set(REEF_L2_SCORE_POSES_RED_LEFT.toArray(Pose2d[]::new));
-
-    table.getStructArrayTopic("reef_l2_red_right", Pose2d.struct)
-        .publish()
-        .set(REEF_L2_SCORE_POSES_RED_RIGHT.toArray(Pose2d[]::new));
-
-    // L1 Blue
-    table.getStructArrayTopic("reef_l1_blue", Pose2d.struct)
-        .publish()
-        .set(REEF_L1_SCORE_POSES_BLUE.toArray(Pose2d[]::new));
-
-    // L1 Red
-    table.getStructArrayTopic("reef_l1_red", Pose2d.struct)
-        .publish()
-        .set(REEF_L1_SCORE_POSES_RED.toArray(Pose2d[]::new));
   }
 
   /**
@@ -259,8 +170,6 @@ public class AutoCommands {
   public Command scoreCoralLevel3Left(boolean allowScoreWithoutTag) {
     return autoScoreCoral(
         armSubsystem::moveToLevel3,
-          REEF_L3_SCORE_POSES_RED_RIGHT,
-          REEF_L3_SCORE_POSES_BLUE_RIGHT,
           DISTANCE_TARGET_L3,
           LATERAL_TARGET_L3_LEFT,
           highCameraForLeft,
@@ -277,8 +186,6 @@ public class AutoCommands {
   public Command scoreCoralLevel3Right(boolean allowScoreWithoutTag) {
     return autoScoreCoral(
         armSubsystem::moveToLevel3,
-          REEF_L3_SCORE_POSES_RED_RIGHT,
-          REEF_L3_SCORE_POSES_BLUE_RIGHT,
           DISTANCE_TARGET_L3,
           LATERAL_TARGET_L3_RIGHT,
           highCameraForRight,
@@ -288,14 +195,11 @@ public class AutoCommands {
 
   private Command autoScoreCoral(
       Runnable armMethod,
-      List<Pose2d> redPoses,
-      List<Pose2d> bluePoses,
       Distance targetDistance,
       Distance lateralTarget,
       PhotonCamera highCamera,
       boolean allowScoreWithoutTag,
       Color ledColor) {
-    var driveToReef = new DriveToNearestPose(drivetrain, () -> drivetrain.getState().Pose, redPoses, bluePoses);
     var alignToReef = new AlignToReefCommand(
         drivetrain,
         alignmentSubsystem,
